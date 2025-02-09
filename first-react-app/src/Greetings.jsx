14d0399fe76ea4d48a31bdc429de23dd377c1514
phatommobb <nickoluoch1130@gmail.com>
2025-02-09 11:25:01 +0300

    react code

--- first-react-app/src/Greetings.jsx
+++ first-react-app/src/Greetings.jsx
@@ -1,5 +1,15 @@
 function Greeting() {
-  return <h1>My favourite food is chapaii bruv!!!</h1>;
+  return (
+    <div>
+      <h1>Test title</h1>
+      <svg>
+        <circle cx="25" cy="75" r="20" stroke="green" strokeWidth="2" />
+      </svg>
+      <form>
+        <input type="text" />
+      </form>
+    </div>
+  );
 }
 
 export default Greeting;
