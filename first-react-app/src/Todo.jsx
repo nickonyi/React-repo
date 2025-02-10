6361c56553d738554362fc0f3e276a6b523b0357
phatommobb <nickoluoch1130@gmail.com>
2025-02-10 17:41:41 +0300

    react code

--- first-react-app/src/Todo.jsx
+++ first-react-app/src/Todo.jsx
@@ -1,1 +1,17 @@
-export default function TodoList() {}
+export default function TodoList() {
+  return (
+    <>
+      <h1>Heddy Lammars Todo</h1>
+      <img
+        src="https://i.imgur.com/yXOvdOSs.jpg"
+        alt="Hedy Lamarr"
+        className="photo"
+      />
+      <ul>
+        <li>Invent new traffic lights</li>
+        <li>Reherse a movie scene</li>
+        <li>Improve the spectrum technology</li>
+      </ul>
+    </>
+  );
+}
