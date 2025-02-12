7d05f054a85cc2690872c915e46ec74a139c906a
phatommobb <nickoluoch1130@gmail.com>
2025-02-12 14:38:28 +0300

    react code

--- first-react-app/src/App.jsx
+++ first-react-app/src/App.jsx
@@ -1,15 +1,28 @@
 import './App.css';
 
+function ListItem(props) {
+  return <li>{props.animal}</li>;
+}
+
+function List(props) {
+  return (
+    <ul>
+      {props.animalsList.map((animal) => {
+        return animal.startsWith('L') ? (
+          <ListItem key={animal} animal={animal} />
+        ) : null;
+      })}
+    </ul>
+  );
+}
+
 function App() {
-  const animals = ['Lion', 'Cow', 'Snake', 'Lizard'];
+  const animals = ['Lion', 'Cow', 'Snake', 'Lizard', 'Leopard'];
+
   return (
     <>
       <h1>Animals:</h1>
-      <ul>
-        {animals.map((animal) => {
-          return <li key={animal}>{animal}</li>;
-        })}
-      </ul>
+      <List animalsList={animals} />
     </>
   );
 }
