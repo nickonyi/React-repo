c785bbe2571a3010c399d846021fe5670a24a8c6
phatommobb <nickoluoch1130@gmail.com>
2025-02-14 13:11:52 +0300

    react code

--- first-react-app/src/main.jsx
+++ first-react-app/src/main.jsx
@@ -2,10 +2,11 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 //import TodoList from './Todo.jsx';
-import App from './App';
+//import App from './App';
+import List from './List';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <List />
   </StrictMode>
 );
