33ad07215ddb30eeab46adb022c3d426af3d2fb6
phatommobb <nickoluoch1130@gmail.com>
2025-02-15 14:36:41 +0300

    react practice

--- first-react-app/src/List.jsx
+++ first-react-app/src/List.jsx
@@ -2,7 +2,25 @@ import { people } from './data.js';
 import { getImageUrl } from './utils.js';
 
 export default function List() {
-  const listItems = people.map((person) => (
+  const chemists = people.filter((person) => person.profession === 'chemist');
+  const everyoneElse = people.filter(
+    (person) => person.profession !== 'chemist'
+  );
+
+  console.log(chemists);
+  console.log(everyoneElse);
+
+  const chemistGroup = chemists.map((person) => (
+    <li key={person.id}>
+      <img src={getImageUrl(person)} alt={person.name} />
+      <p>
+        <b>{person.name}:</b>
+        {' ' + person.profession + ' '}
+        known for {person.accomplishment}
+      </p>
+    </li>
+  ));
+  const everyoneElseGroup = everyoneElse.map((person) => (
     <li key={person.id}>
       <img src={getImageUrl(person)} alt={person.name} />
       <p>
@@ -14,8 +32,10 @@ export default function List() {
   ));
   return (
     <article>
-      <h1>Scientists</h1>
-      <ul>{listItems}</ul>
+      <h1>Chemists</h1>
+      <ul>{chemistGroup}</ul>
+      <h1>Everyone else</h1>
+      <ul>{everyoneElseGroup}</ul>
     </article>
   );
 }
