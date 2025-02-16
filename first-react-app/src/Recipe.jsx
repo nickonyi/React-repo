08449030314ee26e27fbd92097d999668ea32e7e
phatommobb <nickoluoch1130@gmail.com>
2025-02-16 15:40:07 +0300

    recipe data

--- first-react-app/src/Recipe.jsx
+++ first-react-app/src/Recipe.jsx
@@ -1,19 +1,30 @@
 import { recipes } from './recipedata';
 
+function RecipeComp({ id, name, ingredients }) {
+  return (
+    <div key={id}>
+      <h2>{name}</h2>
+      <ul>
+        {ingredients.map((ingredient) => (
+          <li key={ingredient}>{ingredient}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
 export default function RecipeList() {
   return (
     <div>
       <h1>Recipes</h1>
       <ul>
         {recipes.map((recipe) => (
-          <div key={recipe.id}>
-            <h2>{recipe.name}</h2>
-            <ul>
-              {recipe.ingredients.map((ingredient) => (
-                <li key={ingredient}>{ingredient}</li>
-              ))}
-            </ul>
-          </div>
+          <RecipeComp
+            key={recipe.id}
+            id={recipe.id}
+            name={recipe.name}
+            ingredients={recipe.ingredients}
+          />
         ))}
       </ul>
     </div>
