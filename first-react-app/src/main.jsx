78faee00607945cc07d639477b1b08a5b7ad840e
phatommobb <nickoluoch1130@gmail.com>
2025-02-16 15:45:42 +0300

    recipe data

--- first-react-app/src/main.jsx
+++ first-react-app/src/main.jsx
@@ -1,13 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import RecipeList from './Recipe';
+import Poem from './Poem';
+//import RecipeList from './Recipe';
 //import TodoList from './Todo.jsx';
 //import App from './App';
 //import List from './List';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RecipeList />
+    <Poem />
   </StrictMode>
 );
