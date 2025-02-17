5341938d23a4d18ab1269df3053425d2dc8e8fff
phatommobb <nickoluoch1130@gmail.com>
2025-02-17 15:01:17 +0300

    react code

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,57 +1,61 @@
 import { getImageUrl } from '../data/utils';
 
+function Profile({ name, imageId, size, profession, awards, discovery }) {
+  return (
+    <section className="profile">
+      <h2>{name}</h2>
+      <img
+        className="avatar"
+        src={getImageUrl(imageId)}
+        alt={name}
+        width={size}
+        height={size}
+      />
+      <ul>
+        <li>
+          <b>Profession: </b>
+          {profession}
+        </li>
+        <li>
+          <b>Awards: {awards.length} </b>({awards.join(',')})
+        </li>
+        <li>
+          <b>Discovered: </b>
+          {discovery}
+        </li>
+      </ul>
+    </section>
+  );
+}
+
 export default function Gallery() {
   return (
     <div>
       <h1>Notable Scientists</h1>
-      <section className="profile">
-        <h2>Maria Skłodowska-Curie</h2>
-        <img
-          className="avatar"
-          src={getImageUrl('szV5sdG')}
-          alt="Maria Skłodowska-Curie"
-          width={70}
-          height={70}
+      <div>
+        <Profile
+          name="Maria Skłodowska-Curie"
+          imageId="szV5sdG"
+          size={70}
+          profession="physicist and chemist"
+          awards={[
+            'Nobel Prize in Physics',
+            'Nobel Prize in Chemistry',
+            'Davy Medal',
+            'Matteucci Medal',
+          ]}
+          discovery="polonium (chemical element)"
         />
-        <ul>
-          <li>
-            <b>Profession: </b>
-            physicist and chemist
-          </li>
-          <li>
-            <b>Awards: 4 </b>
-            (Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal,
-            Matteucci Medal)
-          </li>
-          <li>
-            <b>Discovered: </b>
-            polonium (chemical element)
-          </li>
-        </ul>
-      </section>
-      <section className="profile">
-        <h2>Katsuko Saruhashi</h2>
-        <img
-          className="avatar"
-          src={getImageUrl('YfeOqp2')}
-          alt="Katsuko Saruhashi"
-          width={70}
-          height={70}
+        <Profile
+          name="Katsuko Saruhashi"
+          imageId="YfeOqp2"
+          size={70}
+          profession=" geochemist"
+          awards={['Miyake Prize for geochemistry', 'Tanaka Prize']}
+          discovery="a method for measuring carbon dioxide in
+              seawater"
         />
-        <ul>
-          <li>
-            <b>Profession: </b>
-            geochemist
-          </li>
-          <li>
-            <b>Awards: 2 </b>
-            (Miyake Prize for geochemistry, Tanaka Prize)
-          </li>
-          <li>
-            <b>Discovered: </b>a method for measuring carbon dioxide in seawater
-          </li>
-        </ul>
-      </section>
+      </div>
     </div>
   );
 }
--- src/data/utils.js
+++ src/data/utils.js
@@ -1,3 +1,3 @@
-export function getImageUrl(person) {
-  return 'https://i.imgur.com/' + person.imageId + 's.jpg';
+export function getImageUrl(imageId, size = 's') {
+  return 'https://i.imgur.com/' + imageId + size + '.jpg';
 }
