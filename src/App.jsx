e9dd1c31ddcd2937f3effa23b96dfc419ae82816
phatommobb <nickoluoch1130@gmail.com>
2025-02-18 18:06:16 +0300

    new react code

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
 import './styles/App.css';
+import { useState } from 'react';
+
+const colors = ['pink', 'green', 'blue', 'yellow', 'purple', 'red'];
 
 function App() {
-  return <div></div>;
+  const [backgroundColor, setBackgroundColor] = useState(colors[0]);
+
+  const onButtonClick = (color) => () => {
+    setBackgroundColor(color);
+  };
+
+  return (
+    <div className="App" style={{ backgroundColor }}>
+      {colors.map((color) => (
+        <button
+          key={color}
+          type="button"
+          className={backgroundColor === color ? 'selected' : ''}
+          onClick={onButtonClick(color)}
+        >
+          {color}
+        </button>
+      ))}
+    </div>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './styles/index.css';
-import Gallery from './components/Gallery';
+//import Gallery from './components/Gallery';
 //import Profile from './components/Profile';
 //import Poem from './Poem';
 //import RecipeList from './Recipe';
 //import TodoList from './Todo.jsx';
-//import App from './App';
+import App from './App';
 //import List from './List';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Gallery />
+    <App />
   </StrictMode>
 );
