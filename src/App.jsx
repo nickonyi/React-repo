d440c07d583cfe064e964fc2c016e638fe5fa383
phatommobb <nickoluoch1130@gmail.com>
2025-02-18 18:08:23 +0300

    kilometer

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   return (
     <div>
       <Button text="click me!" color="red" fontSize={12} />
-      <Button text="do not click me!" color="blue" fontSize={20} />
+      <Button text="do not kilometer click me!" color="blue" fontSize={20} />
       <Button text="click me!" color="red" fontSize={12} />
     </div>
   );
