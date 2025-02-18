d4ef67542e2f02b530a22c82cf11d009c7ee3cbe
phatommobb <nickoluoch1130@gmail.com>
2025-02-18 18:12:33 +0300

    nice code

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,11 @@ function App() {
   return (
     <div>
       <Button text="click me!" color="red" fontSize={12} />
-      <Button text="do not kilomexxxter click me!" color="blue" fontSize={20} />
+      <Button
+        text="do not kilomexxxdter click me!"
+        color="blue"
+        fontSize={20}
+      />
       <Button text="click me!" color="red" fontSize={12} />
     </div>
   );
