a0ad7382510d3202201d314caa5d16aecef77d85
unknown <nickoluoch1130@gmail.com>
2025-02-20 15:05:49 +0300

    states in components

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,61 +1,24 @@
-import { getImageUrl } from '../data/utils';
+import { useState } from "react";
+import { sculptureList } from "../data/sculpturedata";
 
-function Profile({ name, imageId, size, profession, awards, discovery }) {
-  return (
-    <section className="profile">
-      <h2>{name}</h2>
-      <img
-        className="avatar"
-        src={getImageUrl(imageId)}
-        alt={name}
-        width={size}
-        height={size}
-      />
-      <ul>
-        <li>
-          <b>Profession: </b>
-          {profession}
-        </li>
-        <li>
-          <b>Awards: {awards.length} </b>({awards.join(',')})
-        </li>
-        <li>
-          <b>Discovered: </b>
-          {discovery}
-        </li>
-      </ul>
-    </section>
-  );
-}
+export default function Gallery(){
+    const [index,setIndex] = useState(0);
 
-export default function Gallery() {
-  return (
-    <div>
-      <h1>Notable Scientists</h1>
-      <div>
-        <Profile
-          name="Maria Skłodowska-Curie"
-          imageId="szV5sdG"
-          size={70}
-          profession="physicist and chemist"
-          awards={[
-            'Nobel Prize in Physics',
-            'Nobel Prize in Chemistry',
-            'Davy Medal',
-            'Matteucci Medal',
-          ]}
-          discovery="polonium (chemical element)"
-        />
-        <Profile
-          name="Katsuko Saruhashi"
-          imageId="YfeOqp2"
-          size={70}
-          profession=" geochemist"
-          awards={['Miyake Prize for geochemistry', 'Tanaka Prize']}
-          discovery="a method for measuring carbon dioxide in
-              seawater"
-        />
-      </div>
-    </div>
-  );
+    const handleClick = ()=> {
+        setIndex(index + 1);
+    }
+
+    let sculputre = sculptureList[index];
+
+    return (
+        <>
+        <button onClick={handleClick}>Next</button>
+        <h2>
+            <i>{sculputre.name}</i> by {sculputre.artist}
+        </h2>
+        <h3>({index + 1} of {sculptureList.length})</h3>
+        <img src={sculputre.url} alt={sculputre.alt} />
+        <p>{sculputre.description}</p>
+        </>
+    )
 }
