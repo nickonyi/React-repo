d1879ccd7a8352cb0eaef77de4640006955a6550
phatommobb <nickoluoch1130@gmail.com>
2025-02-25 17:30:44 +0300

    state structuring

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -27,8 +27,8 @@ export default function Accordion() {
         The name comes from <span lang="kk-KZ">алма</span>, the Kazakh word for
         "apple" and is often translated ass "full of apples". In fact, the
         region surrounding Almaty is thought to be the ancestral home of the
-        apple, and the wild <i lang="la">Malus sieversii</i> is considered a
-        likely candidate for the ancestor of the modern domestic apple.
+        apple, and the wild <i lang="la">Malus spane;ieversii</i> is considered
+        a likely candidate for the ancestor of the modern domestic apple.
       </Panel>
     </>
   );
