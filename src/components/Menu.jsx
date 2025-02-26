a4417776b8b80ae82f731f864c11e30e6aab1bce
phatommobb <nickoluoch1130@gmail.com>
2025-02-26 14:40:32 +0300

    structuring state

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -8,7 +8,24 @@ const initialItems = [
 
 export default function Menu() {
   const [items, setItems] = useState(initialItems);
-  const [selectedItem, setSelectedItem] = useState(items[0]);
+  const [selectedId, setSelectedId] = useState(0);
+
+  const selectedItem = items.find((item) => item.id === selectedId);
+
+  const handleItemChange = (id, e) => {
+    setItems(
+      items.map((item) => {
+        if (item.id === id) {
+          return {
+            ...item,
+            title: e.target.value,
+          };
+        } else {
+          return item;
+        }
+      })
+    );
+  };
 
   return (
     <>
@@ -16,9 +33,12 @@ export default function Menu() {
       <ul>
         {items.map((item) => (
           <li key={item.id}>
-            {item.title}
+            <input
+              value={item.title}
+              onChange={(e) => handleItemChange(item.id, e)}
+            />
             {'  '}
-            <button onClick={() => setSelectedItem(item)}>choose</button>
+            <button onClick={() => setSelectedId(item.id)}>choose</button>
           </li>
         ))}
       </ul>
