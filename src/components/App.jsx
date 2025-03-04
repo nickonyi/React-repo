32c11a9ad0393c2fbc140c321db4c616725d9150
phatommobb <nickoluoch1130@gmail.com>
2025-03-04 14:35:31 +0300

    use effect code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,25 @@
+import { useState } from 'react';
 import ChatRoom from './ChatRoom';
 
-export default function App() {}
+export default function App() {
+  const [roomId, setRoomId] = useState('general');
+  const [show, setShow] = useState(false);
+
+  return (
+    <>
+      <label>
+        Choose the ChatRoom:{'  '}
+        <select value={roomId} onChange={(e) => setRoomId(e.target.value)}>
+          <option value="general">general</option>
+          <option value="travel">travel</option>
+          <option value="music">music</option>
+        </select>
+      </label>
+      <button onClick={() => setShow(!show)}>
+        {show ? 'Close chat' : 'Open chat'}
+      </button>
+      {show && <hr />}
+      {show && <ChatRoom roomId={roomId} />}
+    </>
+  );
+}
