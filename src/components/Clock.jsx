2c9d624947a6470c5372ead755b7aa2f70941ba8
unknown <nickoluoch1130@gmail.com>
2025-05-12 23:17:34 +0300

    code

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -16,6 +16,6 @@ export default function Clock (){
   
 
     return (
-        <p>{counter} seconds have pgdghhddhdhddhdgassed.</p>
+        <p>{counter} seconds have pgdghhassssggsggsddhdhddhdgassed.</p>
     )
 }
